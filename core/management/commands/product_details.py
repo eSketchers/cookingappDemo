40246a3bf83789fb756c2bc14c3c6f385f48921d
Salman Ahmed <salman.esketchers@gmail.com>
2018-07-02 16:09:19 +0500

    Changed query params to get urls.

--- core/management/commands/product_details.py
+++ core/management/commands/product_details.py
@@ -18,7 +18,7 @@ class Command(BaseCommand):
         user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
         headers = {'User-Agent': user_agent, }
         try:
-            _store_urls = StoreUrl.objects.filter(user_id = user_id)
+            _store_urls = StoreUrl.objects.filter(user_id = user_id, get_product=False)
             _urls_count = _store_urls.count()
 
             for url_obj in _store_urls:
